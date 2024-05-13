c15d3d2d75f669ecefcdf4b408aa15e7971fc7e6
atif <atif_hossain@live.com>
2024-05-13 19:43:23 +0530

    chore: updated scss template to include vars.scss file

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ export { default } from './${componentName}';
 
 const wrapperClassName = `wrapper`
 const cssFilePath = `${componentDir}/${componentName}.module.scss`;
-const cssTemplate = `/* Styles for ${componentName} */\n\n.${wrapperClassName} {\n  /* Add your styles here */\n}`;
+const cssTemplate = `@import "~utils/vars.scss";\n\n.${wrapperClassName} {\n  /* Add your styles here */\n}`;
 
 
 logIntro({
