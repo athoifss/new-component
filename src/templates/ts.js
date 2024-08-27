7a67cfec1aba8cde62dc5711abb62847e1fb86cf
atif <atif_hossain@live.com>
2024-08-27 11:40:48 +0530

    chore: changed jsx template

--- src/templates/ts.js
+++ src/templates/ts.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import "./COMPONENT_NAME.module.scss"
+import style from "./COMPONENT_NAME.module.scss"
 
 export default function COMPONENT_NAME() {
-  return <div></div>;
+  return <div className={style.wrapper}></div>;
 }
 
 
